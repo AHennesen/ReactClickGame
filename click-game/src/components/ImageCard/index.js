5e32e49add675b3f11dede5653a0a5cd9ba3833d
Andrew H <ahennesen@gmail.com>
2019-06-28 14:59:52 -0400

    added components to src and put in the image card

---
+++ click-game/src/components/ImageCard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function ImageCard (props) {
+    return (
+        <img className = 'card' alt = {props.id} onClick={props.imageClick} />
+    )
+}
